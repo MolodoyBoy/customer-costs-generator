f9365caffff6ca1ad391e68e2aa9105b5979edbf
o.chukhlian <oleg.chukhlian@pitchbook.com>
2025-05-13 17:54:35 +0300

    fix kafka publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
 
 val dockerHubUsername: String by project
